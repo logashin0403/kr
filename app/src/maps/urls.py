69ea208abc674c96a45a3fe8e2d903dfa982bf04
Eduard <45480678+logashin0403@users.noreply.github.com>
2023-02-27 20:07:46 +0300

    Delete app directory

--- app/src/maps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import add_message, grounds_list
-
-
-urlpatterns = [
-    path('<str:sport_type>/', grounds_list, name='grounds_list'),
-    path('<str:sport_type>/<int:ground_id>/add_message/', add_message, name="add_message"),
-
-]
--- app/src/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    """custom user"""
-    email = models.EmailField(verbose_name="Почта", max_length=150, unique=True)
-    updated_at = models.DateTimeField(auto_now=True)
